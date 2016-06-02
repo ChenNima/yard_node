48d3573753f4d86ade6f30dc9b8284db64ce5ce0
Yifei Chen <yifei.chen@careerbuilder.com>
2016-06-02 10:55:28 +0800

    test

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -9,44 +9,53 @@ angular.module('myApp', [
         'restangular'
     ])
     .controller('HomeController',
-            ['$scope',
+        ['$scope',
             'HitService',
             'Restangular',
-            function($scope, HitService,Restangular) {
-        //HitService.count()
-        //    .then(function(data) {
-        //        $scope.hits = data;
-        //    });
+            function ($scope, HitService, Restangular) {
+                //HitService.count()
+                //    .then(function(data) {
+                //        $scope.hits = data;
+                //    });
+                var sendData;
 
-        setInterval(function(){
-            Restangular.one('/get_sms').get()
-                .then(function (data){
-                    $scope.datas = data.smsArray.reverse();
-                });
-        },1000);
+                setInterval(function () {
+                    refresh();
+                }, 1000);
 
-        $scope.registerHit = function() {
-            Restangular.one('/hit').post()
-                .then(function (data){
-                    $scope.hits = data.hits;
-                });
+                $scope.registerHit = function () {
+                    Restangular.one('/hit').post()
+                        .then(function (data) {
+                            $scope.hits = data.hits;
+                        });
+                };
 
+                var refresh = function(){
+                    Restangular.one('/get_sms').get()
+                        .then(function (data){
+                            $scope.datas = data.smsArray.reverse();
+                        });
+                };
 
+                var deepCopy= function(source) {
+                    var result={};
+                    for (var key in source) {
+                        result[key] = typeof source[key]==='object'? deepCoyp(source[key]): source[key];
+                    }
+                    return result;
+                };
 
-            //HitService.doHit()
-            //    .then(function (data){
-            //    $scope.hits = data;
-            //});
-        }
-
-                $scope.smsTest = function() {
+                $scope.smsTest = function () {
                     var myDate = new Date();
-                    var time = myDate.getHours()+":"+myDate.getMinutes()+":"+myDate.getSeconds();
+                    var time = myDate.getHours() + ":" + myDate.getMinutes() + ":" + myDate.getSeconds();
                     $scope.data.date = time;
-                    Restangular.one('/').post('add_sms', {data:$scope.data})
+                    sendData = deepCopy($scope.data);
+                    $scope.data.content = "";
+                    Restangular.one('/').post('add_sms', {data: sendData})
                         .then(function (data) {
                             $scope.datas = data.smsArray.reverse();
-                            $scope.data.content="";
                         });
-                }
-    }]);
+                };
+
+                refresh();
+            }]);
