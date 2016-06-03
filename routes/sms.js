a430056fda9ecd27af418e40cb29cfd3c341522e
Yifei Chen <yifei.chen@careerbuilder.com>
2016-06-03 12:28:08 +0800

    add css

--- routes/sms.js
+++ routes/sms.js
@@ -12,16 +12,35 @@ var chatLog = mongoose.model('chat_log', chatLogSchema);
 
 var smsArray = [];
 
+//var refresh = function(callback){
+//    chatLog.find(function (err, logs) {
+//        if (err) return console.error(err);
+//        smsArray = [];
+//        for (var index in logs){
+//            smsArray.push({data:logs[index]._doc});
+//        }
+//        if(callback){
+//            callback();
+//        }
+//    });
+//};
+
 var refresh = function(callback){
-    chatLog.find(function (err, logs) {
-        if (err) return console.error(err);
-        smsArray = [];
-        for (var index in logs){
-            smsArray.push({data:logs[index]._doc});
-        }
-        if(callback){
-            callback();
-        }
+    chatLog.count({},function (err, count){
+        chatLog
+            .find()
+            .skip(count-25)
+            .limit(25)
+            .exec(function (err, logs) {
+                if (err) return console.error(err);
+                smsArray = [];
+                for (var index in logs){
+                    smsArray.push({data:logs[index]._doc});
+                }
+                if(callback){
+                    callback();
+                }
+            });
     });
 };
 
@@ -50,7 +69,9 @@ exports.addNew = function(req, res) {
         console.log(test.name+"saved");
     });
     smsArray.push(req.body);
-    res.send(200, {
-        smsArray: smsArray
+    refresh(function(){
+        res.send(200, {
+            smsArray: smsArray
+        });
     });
 };
