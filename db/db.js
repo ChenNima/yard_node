282402014ae16f361bf67458165a5069a21da185
Yifei Chen <yifei.chen@careerbuilder.com>
2016-06-22 16:32:05 +0800

    db tweak

--- db/db.js
+++ db/db.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 exports.connect = function(){
-    mongoose.connect('mongodb://yifei.chen:FFff1122@ds021663.mlab.com:21663/mrc_mongo');
+    //mongoose.connect('mongodb://yifei.chen:FFff1122@ds021663.mlab.com:21663/mrc_mongo');
+    mongoose.connect('mongodb://mrchen.pub:27017/mrc_mongo');
     var db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
     db.once('open', function (callback) {
