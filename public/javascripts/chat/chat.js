246b976488e576212599427c0eb735857b6a6039
Yifei Chen <yifei.chen@careerbuilder.com>
2016-07-15 13:11:18 +0800

    bug fixed

--- public/javascripts/chat/chat.js
+++ public/javascripts/chat/chat.js
@@ -77,7 +77,7 @@ angular.module('myApp')
                 var deepCopy = function (source) {
                     var result = {};
                     for (var key in source) {
-                        result[key] = typeof source[key] === 'object' ? deepCoyp(source[key]) : source[key];
+                        result[key] = typeof source[key] === 'object' ? deepCopy(source[key]) : source[key];
                     }
                     return result;
                 };
