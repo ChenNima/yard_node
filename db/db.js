7a39a14fe897f7f55331389d0cd667e9ec09a068
yifei-chen <Yifei.Chen@careerbuilder.com>
2017-01-17 12:41:15 +0800

    change db

--- db/db.js
+++ db/db.js
@@ -6,7 +6,8 @@ var kSplitJob = require('../routes/job/k-split-job');
 
 exports.connect = function(){
     //mongoose.connect('mongodb://yifei.chen:FFff1122@ds021663.mlab.com:21663/mrc_mongo');
-    mongoose.connect('mongodb://mrchen.pub:27017/mrc_mongo');
+    mongoose.connect('mongodb://yifei:ffffff@mrchen.pub:27028');
+    //mongoose.connect('mongodb://mrchen.pub:27017/mrc_mongo');
     var db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
     db.once('open', function (callback) {
